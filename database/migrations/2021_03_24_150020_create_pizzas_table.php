44dcc7b9e6bc77511251f54e6c2fa9e1f9286a76
Simone Bresci <simone.bresci.05@gmail.com>
2021-03-24 16:23:38 +0100

    model + articoli di prova + metodo fillable

---
+++ database/migrations/2021_03_24_150020_create_pizzas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePizzasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pizzas', function (Blueprint $table) {
+            $table->id();
+            $table->longText('img_path');
+            $table->string('name', 255);
+            $table->longText('ingredients');
+            $table->float('price', 6, 2);
+            $table->longText('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pizzas');
+    }
+}
